2d51dd6110ffcb5e8b34026f3f8d5ed8c37eeb0c
Yaroslav Novikov <arhdomain@gmail.com>
2023-12-16 12:58:42 +0300

    подстветка кода

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,12 +1,56 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from "axios"
+import 'highlight.js/lib/common';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
+
+export default {
+    data() {
+        return {
+            posts: [],
+            isLoaded: false,
+        }
+    },
+    components: {
+    },
+    methods: {
+        highLight() {
+            document.querySelectorAll('pre code').forEach((el) => {
+                el.innerHTML = el.innerHTML.
+                    replace(/&/g, "&amp;").
+                    replace('<code>', '').
+                    replace('</code>', '').
+                    replace(/</g, "&lt;").
+                    replace(/>/g, "&gt;").
+                    replace(/"/g, "&quot;").
+                    replace(/'/g, "&#039;")
+                hljs.highlightElement(el);
+            });
+        },
+        async fetchPosts() {
+            try {
+                const response = await axios.get('http://192.168.100.18:8000/api/v1/posts/post/')
+                if (response.status == 200) {
+                    this.posts = response.data
+                    this.isLoaded = true
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        },
+    },
+    async mounted() {
+        await this.fetchPosts()
+        this.highLight()
+    }
+}
 </script>
 
 <template>
-
+    <div v-if="isLoaded">
+        <h1>{{ posts[1].title }}</h1>
+        <p v-html="posts[1].text"></p>
+    </div>
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
